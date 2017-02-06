8cc7750c25119282b1c23be6beed647f148b967e
StefanAleksik <stefan_aleksik@live.com>
2017-02-06 04:11:04 +0100

    We have Spotify data, next is pars in to csv and send it to a web server. In the meantime we need to connect DB with spotify entry.

--- routes/spotifycallback.js
+++ routes/spotifycallback.js
@@ -5,7 +5,6 @@ var express = require('express');
 var router = express.Router();
 var request = require('request'); // "Request" library
 var querystring = require('querystring');
-// credentials are optional
 var SpotifyWebApi = require('spotify-web-api-node');
 var spotifyApi = new SpotifyWebApi({
     clientId : '4feeba43e6634d019efb83b3fdc7fe31',
@@ -15,32 +14,51 @@ var spotifyApi = new SpotifyWebApi({
 router.get('/', function(req, res, next) {
     res.send(req.query.code);
     spotifyApi.authorizationCodeGrant(req.query.code).then(function (data) {
-        /*console.log('The token expires in ' + data['expires_in']);
-        console.log(data.body.access_token);
-        var toke = data['refresh_token'];
-        console.log('The refresh token is ' + toke);
-        for (var i in data){
-            console.log(data[i], i);
-        }*/
         spotifyApi.setAccessToken(data.body.access_token);
         return spotifyApi.getMe();
-
     }).then(function (data) {
+ /*       spotifyApi.getMySavedAlbums({
+            limit : 50
+        })
+            .then(function(albums) {
+                // Output items
+                console.log(items.body.items);
+                console.log(items.body.items.length);
+                console.log('asdfg')
+            }, function(err) {
+                console.log('Something went wrong!', err);
+            });*/
+        spotifyApi.getFollowedArtists({ limit : 50 })
+            .then(function(artist) {
+              /*  for (var i =0; i < artist.body.artists.items.length; i++){
+                    console.log('name: ' + artist.body.artists.items[i].name + ', spotify id: ' + artist.body.artists.items[i].id + ', popularity: ' + artist.body.artists.items[i].popularity);
+                }*/
+            }, function(err) {
+                console.log('Something went wrong!', err);
+            });
+        spotifyApi.getMySavedTracks({limit : 50})
+            .then(function(track) {
+            /*    for (var i =0; i < track.body.items.length; i++){
+                 console.log('Song name: ' + track.body.items[i].track.name + ', Song spotify id: ' + track.body.items[i].track.id + ', Song album-name: ' + track.body.items[i].track.album.name + ', Album  spotify-id: ' + track.body.items[i].track.album.id
+                 + ', Album artist  spotify-id: ' + track.body.items[i].track.artists[0].id + ', Album artist-name: ' + track.body.items[i].track.artists[0].name);
+                 }
+            */}, function(err) {
+                console.log('Something went wrong!', err);
+            });
+        spotifyApi.getUserPlaylists(data.body.id)
+            .then(function(playlist) {
+                for (var i =0; i < playlist.body.items.length; i++){
+                    console.log('Playlist name: ' + playlist.body.items[i].name + ', Playlist ID: ' + playlist.body.items[i].id);
+                }
+            },function(err) {
+                console.log('Something went wrong!', err);
+            });
+        //return spotifyApi.getUserPlaylists(data.body.id)
 
-        //console.log(data);
-        return spotifyApi.getUserPlaylists(data.body.id)
-
-    }).then(function (data) {
-        console.log(data.body)
     }), function (err) {
         res.status(err.code);
         res.send(err.message);
     }
 
 });
-
-
-
-
-
 module.exports = router;
--- routes/spotifylogin.js
+++ routes/spotifylogin.js
@@ -23,7 +23,7 @@ router.get('/', function(req, res) {
     res.cookie(stateKey, state);
 
     // your application requests authorization
-    var scope = 'user-read-private user-read-email user-library-read';
+    var scope = 'user-read-private user-read-email user-library-read user-follow-read';
     res.redirect('https://accounts.spotify.com/authorize?' +
         querystring.stringify({
             response_type: 'code',
