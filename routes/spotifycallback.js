ad3277eb8084130ee56e54fbbc9ab6772ef93cce
StefanAleksik <stefan_aleksik@live.com>
2017-02-07 04:05:10 +0100

    We have spotify json ready to be parsed, we can retrive all of the data.

--- routes/spotifycallback.js
+++ routes/spotifycallback.js
@@ -11,47 +11,62 @@ var spotifyApi = new SpotifyWebApi({
     clientSecret : 'b8401d1bb1b94c1280faa4f54d77da27',
     redirectUri : 'http://localhost:3000/spotifycallback'
 });
-var sendData = require('./sendData');
+//var sendData = require('./sendData');
 router.get('/', function(req, res, next) {
     res.send(req.query.code);
     spotifyApi.authorizationCodeGrant(req.query.code).then(function (data) {
         spotifyApi.setAccessToken(data.body.access_token);
         return spotifyApi.getMe();
     }).then(function (data) {
-
-        /*spotifyApi.getFollowedArtists({ limit : 50 })
+        spotifyApi.getFollowedArtists({ limit : 50 })
          .then(function(artist) {
-         for (var i =0; i < artist.body.artists.items.length; i++){
-         console.log('name: ' + artist.body.artists.items[i].name + ', spotify id: ' + artist.body.artists.items[i].id + ', popularity: ' + artist.body.artists.items[i].popularity);
-         }
-         sendData.sendUserPlaylists(data)
-         }, function(err) {
+             /*var limit = 50;*/
+            /* var offset_offset = 0;*/
+             var total = artist.body.artists.total;
+             var counter = 1;
+             /*var uplimit = Math.ceil(total/limit);*/
+             var followedArtists ={spotify_id: data.body.id ,artists: []};
+             if (total <= 50){
+                 for (var i =0; i < artist.body.artists.items.length; i++){
+                     //console.log('name: ' + artist.body.artists.items[i].name + ', spotify id: ' + artist.body.artists.items[i].id + ', popularity: ' + artist.body.artists.items[i].popularity);
+                    followedArtists.artists.push({artist_name: artist.body.artists.items[i].name, artist_spotify_id: artist.body.artists.items[i].id, artist_popularity: artist.body.artists.items[i].popularity});
+                 counter++;
+                    if (counter == artist.body.artists.items.length){
+                     console.log(followedArtists);
+                 }
+                 }
+             }
+                  }, function(err) {
          console.log('Something went wrong!', err);
-         });*/
+         });
         spotifyApi.getMySavedTracks({limit : 50, offset : 0})
             .then(function(track) {
-                if (track.body.total <= 50){
-                    for (var i =0; i < track.body.items.length; i++){
-                        console.log('Song name: ' + track.body.items[i].track.name + ', Song spotify id: ' + track.body.items[i].track.id + ', Song album-name: ' + track.body.items[i].track.album.name + ', Album  spotify-id: ' + track.body.items[i].track.album.id
-                            + ', Album artist  spotify-id: ' + track.body.items[i].track.artists[0].id + ', Album artist-name: ' + track.body.items[i].track.artists[0].name);
+                var limit = 50;
+                var counter = 1;
+                var offset_offset = 0;
+                var total = track.body.total;
+                var uplimit = Math.ceil(total/limit);
+                var savedTracks ={spotify_id: data.body.id ,songs: []};
+                if (total <= limit){
+                    for (var i =0; i < tracks.body.items.length; i++){
+                        savedTracks.songs.push({song_name: tracks.body.items[i].track.name, song_spotify_id: tracks.body.items[i].track.id, song_album_name: tracks.body.items[i].track.album.name, album_spotify_id: tracks.body.items[i].track.album.id,
+                            song_artist_name: tracks.body.items[i].track.artists[0].name, song_artist_spotify_id: tracks.body.items[i].track.artists[0].id});
+                        counter++;
+                        if (counter == savedTracks.songs.length){
+                            console.log("Data ready to be send");
+                        }
                     }
                 } else {
-                    var limit = 50;
-                    var offset_offset = 0;
-                    var total = track.body.total;
-                    var uplimit = Math.ceil(total/limit);
-                    var savedTracks ={spotify_id: data.body.id ,songs: []};
-                    for (var ii = 0; ii < uplimit; ii++){
+                   for (var ii = 0; ii < uplimit; ii++){
                         spotifyApi.getMySavedTracks({limit: 50, offset: offset_offset}).then(function (trackss) {
                             console.log('YO YO YO');
                             for (var i =0; i < trackss.body.items.length; i++){
                                 savedTracks.songs.push({song_name: trackss.body.items[i].track.name, song_spotify_id: trackss.body.items[i].track.id, song_album_name: trackss.body.items[i].track.album.name, album_spotify_id: trackss.body.items[i].track.album.id,
                                 song_artist_name: trackss.body.items[i].track.artists[0].name, song_artist_spotify_id: trackss.body.items[i].track.artists[0].id});
-                            };
+                            }
                             if (total == savedTracks.songs.length){
                                 console.log("Data ready to be send");
-                            };
-
+                            }
                         }, function(err) {
                             console.log('Something went wrong!', err);
                         });
