5907b2714d3769531269fad4168e00a802ffbe6c
StefanAleksik <stefan_aleksik@live.com>
2017-02-07 02:31:42 +0100

    We have spotify json ready to be parsed, we can retrive all of the data.

---
+++ routes/sendData.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Stefan Aleksik on 06.2.2017.
+ */
+
+
+exports.sendUserPlaylists = function(data){
+
+}
--- routes/spotifycallback.js
+++ routes/spotifycallback.js
@@ -11,54 +11,68 @@ var spotifyApi = new SpotifyWebApi({
     clientSecret : 'b8401d1bb1b94c1280faa4f54d77da27',
     redirectUri : 'http://localhost:3000/spotifycallback'
 });
+var sendData = require('./sendData');
 router.get('/', function(req, res, next) {
     res.send(req.query.code);
     spotifyApi.authorizationCodeGrant(req.query.code).then(function (data) {
         spotifyApi.setAccessToken(data.body.access_token);
         return spotifyApi.getMe();
     }).then(function (data) {
- /*       spotifyApi.getMySavedAlbums({
-            limit : 50
-        })
-            .then(function(albums) {
-                // Output items
-                console.log(items.body.items);
-                console.log(items.body.items.length);
-                console.log('asdfg')
-            }, function(err) {
-                console.log('Something went wrong!', err);
-            });*/
-        spotifyApi.getFollowedArtists({ limit : 50 })
-            .then(function(artist) {
-                for (var i =0; i < artist.body.artists.items.length; i++){
-                    console.log('name: ' + artist.body.artists.items[i].name + ', spotify id: ' + artist.body.artists.items[i].id + ', popularity: ' + artist.body.artists.items[i].popularity);
-                }
-            }, function(err) {
-                console.log('Something went wrong!', err);
-            });
-        spotifyApi.getMySavedTracks({limit : 50})
-            .then(function(track) {
-                for (var i =0; i < track.body.items.length; i++){
-                 console.log('Song name: ' + track.body.items[i].track.name + ', Song spotify id: ' + track.body.items[i].track.id + ', Song album-name: ' + track.body.items[i].track.album.name + ', Album  spotify-id: ' + track.body.items[i].track.album.id
-                 + ', Album artist  spotify-id: ' + track.body.items[i].track.artists[0].id + ', Album artist-name: ' + track.body.items[i].track.artists[0].name);
-                 }
-            }, function(err) {
-                console.log('Something went wrong!', err);
-            });
-        spotifyApi.getUserPlaylists(data.body.id)
-            .then(function(playlist) {
-                for (var i =0; i < playlist.body.items.length; i++){
-                    console.log('Playlist name: ' + playlist.body.items[i].name + ', Playlist ID: ' + playlist.body.items[i].id);
-                }
-            },function(err) {
-                console.log('Something went wrong!', err);
-            });
-        //return spotifyApi.getUserPlaylists(data.body.id)
 
+        /*spotifyApi.getFollowedArtists({ limit : 50 })
+         .then(function(artist) {
+         for (var i =0; i < artist.body.artists.items.length; i++){
+         console.log('name: ' + artist.body.artists.items[i].name + ', spotify id: ' + artist.body.artists.items[i].id + ', popularity: ' + artist.body.artists.items[i].popularity);
+         }
+         sendData.sendUserPlaylists(data)
+         }, function(err) {
+         console.log('Something went wrong!', err);
+         });*/
+        spotifyApi.getMySavedTracks({limit : 50, offset : 0})
+            .then(function(track) {
+                if (track.body.total <= 50){
+                    for (var i =0; i < track.body.items.length; i++){
+                        console.log('Song name: ' + track.body.items[i].track.name + ', Song spotify id: ' + track.body.items[i].track.id + ', Song album-name: ' + track.body.items[i].track.album.name + ', Album  spotify-id: ' + track.body.items[i].track.album.id
+                            + ', Album artist  spotify-id: ' + track.body.items[i].track.artists[0].id + ', Album artist-name: ' + track.body.items[i].track.artists[0].name);
+                    }
+                } else {
+                    var limit = 50;
+                    var offset_offset = 0;
+                    var total = track.body.total;
+                    var uplimit = Math.ceil(total/limit);
+                    var savedTracks ={spotify_id: data.body.id ,songs: []};
+                    for (var ii = 0; ii < uplimit; ii++){
+                        spotifyApi.getMySavedTracks({limit: 50, offset: offset_offset}).then(function (trackss) {
+                            console.log('YO YO YO');
+                            for (var i =0; i < trackss.body.items.length; i++){
+                                savedTracks.songs.push({song_name: trackss.body.items[i].track.name, song_spotify_id: trackss.body.items[i].track.id, song_album_name: trackss.body.items[i].track.album.name, album_spotify_id: trackss.body.items[i].track.album.id,
+                                song_artist_name: trackss.body.items[i].track.artists[0].name, song_artist_spotify_id: trackss.body.items[i].track.artists[0].id});
+                            };
+                            if (total == savedTracks.songs.length){
+                                console.log("Data ready to be send");
+                            };
+
+                        }, function(err) {
+                            console.log('Something went wrong!', err);
+                        });
+                        offset_offset += limit;
+                    } console.log('HI there');
+                }
+            }, function(err) {
+                console.log('Something went wrong!', err);
+            });
+        /*spotifyApi.getUserPlaylists(data.body.id)
+         .then(function(playlist) {
+         for (var i =0; i < playlist.body.items.length; i++){
+         console.log('Playlist name: ' + playlist.body.items[i].name + ', Playlist ID: ' + playlist.body.items[i].id);
+         }
+         },function(err) {
+         console.log('Something went wrong!', err);
+         });*/
+        //return spotifyApi.getUserPlaylists(data.body.id)
     }), function (err) {
         res.status(err.code);
         res.send(err.message);
     }
-
 });
 module.exports = router;
